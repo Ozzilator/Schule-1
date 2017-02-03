8724d1db78f0595564a9112f4040d72cf5dd4cec
gänseblümchn <felix.zimmermann.1.de@gmail.com>
2017-02-03 12:36:06 +0100

    Update Car.h

--- windowsForms/auto/Car.h
+++ windowsForms/auto/Car.h
@@ -1,1 +1,16 @@
+#pragma once
+using namespace System::Drawing;
+
+ref class Car
+{
+	public:
+		Color farbe;
+		int ortx;
+		int orty;
+		int size;
+	/*Methoden*/
+		Car();
+		void draw(Graphics ^c, int ortx, int orty, int size);
+		void drive();
+};
 
